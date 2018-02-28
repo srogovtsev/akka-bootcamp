f7444b810a68abafb9f86535397d7f2e45901070
Sergey Rogovtsev <srogovtsev@acumatica.com>
2018-02-28 18:29:45 +0300

    Lesson 1.2

--- src/Unit-1/DoThis/ConsoleWriterActor.cs
+++ src/Unit-1/DoThis/ConsoleWriterActor.cs
@@ -11,25 +11,22 @@ namespace WinTail
     {
         protected override void OnReceive(object message)
         {
-            var msg = message as string;
-
-            // make sure we got a message
-            if (string.IsNullOrEmpty(msg))
+            switch (message)
             {
-                Console.ForegroundColor = ConsoleColor.DarkYellow;
-                Console.WriteLine("Please provide an input.\n");
-                Console.ResetColor();
-                return;
+                case Messages.InputError msg:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(msg.Reason);
+                    break;
+                case Messages.InputSuccess msg:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(msg.Reason);
+                    break;
+                default:
+                    Console.WriteLine(message);
+                    break;
             }
 
-            // if message has even # characters, display in red; else, green
-            var even = msg.Length % 2 == 0;
-            var color = even ? ConsoleColor.Red : ConsoleColor.Green;
-            var alert = even ? "Your string had an even # of characters.\n" : "Your string had an odd # of characters.\n";
-            Console.ForegroundColor = color;
-            Console.WriteLine(alert);
             Console.ResetColor();
-
         }
     }
 }
--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -12,31 +12,14 @@ namespace WinTail
         {
             MyActorSystem = ActorSystem.Create("MyActorSystem");
 
-            PrintInstructions();
-
             var consoleWriterActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleWriterActor()));
             var consoleReaderActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(consoleWriterActor)));
 
-            consoleReaderActor.Tell("start");
+            // tell console reader to begin
+            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
 
             // blocks the main thread from exiting until the actor system is shut down
             MyActorSystem.WhenTerminated.Wait();
-        }
-
-        private static void PrintInstructions()
-        {
-            Console.WriteLine("Write whatever you want into the console!");
-            Console.Write("Some lines will appear as");
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.Write(" red ");
-            Console.ResetColor();
-            Console.Write(" and others will appear as");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write(" green! ");
-            Console.ResetColor();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
         }
     }
     #endregion
