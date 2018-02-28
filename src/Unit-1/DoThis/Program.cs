63854df806b4fd81c4bbbef68b8f3cc9cba825bd
Sergey Rogovtsev <srogovtsev@acumatica.com>
2018-02-28 18:35:09 +0300

    Added Serilog logging

--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -1,16 +1,37 @@
 ﻿using System;
 ﻿using Akka.Actor;
+using Serilog;
 
 namespace WinTail
 {
     #region Program
     class Program
     {
+        private const string AkkaConfig = 
+@"akka { 
+    loglevel = DEBUG
+    loggers = [""Akka.Logger.Serilog.SerilogLogger, Akka.Logger.Serilog""]
+    log-config-on-start = off        
+    actor {                
+        debug {  
+              receive = on 
+              autoreceive = on
+              lifecycle = on
+              event-stream = on
+              unhandled = on
+        }
+    }  
+}";
         public static ActorSystem MyActorSystem;
 
         static void Main(string[] args)
         {
-            MyActorSystem = ActorSystem.Create("MyActorSystem");
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.ColoredConsole()
+                .MinimumLevel.Verbose()
+                .CreateLogger();
+
+            MyActorSystem = ActorSystem.Create("MyActorSystem", AkkaConfig);
 
             var consoleWriterActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleWriterActor()));
             var consoleReaderActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(consoleWriterActor)));
